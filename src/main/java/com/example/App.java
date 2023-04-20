6ee46d10a27dc60b392694472f543ec75d396946
Rebeca Coli <rebeca.correia100@gmail.com>
2023-04-20 12:26:51 -0300

    adicionando atividade de banco de dados

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,33 @@
+package com.example;
+import java.sql.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String user = "root";
+        String passwd = "6554";
+        
+        String url = "jdbc:mysql://localhost:3306/locadora";
+        String driveUrl = "com.mysql.jdbc.Driver";
+        try {
+            Class.forName(driveUrl);
+            Connection conn = DriverManager.getConnection(url, user, passwd);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM cliente");
+            
+            while (rs.next()) {
+                String nome = rs.getString("nome");
+                String login = rs.getString("login");
+                System.out.printf("Nome: %s - Login: %s%n", nome, login);
+            }
+            
+            rs.close();
+            stmt.close();
+            conn.close();
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
